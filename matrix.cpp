c6608d559ca2d6941d83ae8a42793883583794a2
Анжелика Демина <angelika@MacBook-Pro-Anzelika-3.local>
2023-08-27 01:57:54 +0300

    3 func

--- matrix.cpp
+++ matrix.cpp
@@ -48,6 +48,150 @@ S21Matrix::S21Matrix(S21Matrix&& other) {
   other.cols_ = 0;
   other.matrix_ = nullptr;
 }
+void S21Matrix::SumMatrix(const S21Matrix& other) {
+  if (rows_ != other.rows_ || cols_ != other.cols_) {
+    throw std::invalid_argument("Different matrix dimentions");
+  }
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      matrix_[i][j] += other.matrix_[i][j];
+    }
+  }
+}
+
+bool S21Matrix::EqMatrix(const S21Matrix& other) {
+  if (rows_ != other.rows_ || cols_ != other.cols_) {
+    return false;
+  }
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      if (matrix_[i][j] != other.matrix_[i][j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+void S21Matrix::SubMatrix(const S21Matrix& other) {
+  if (rows_ != other.rows_ || cols_ != other.cols_) {
+    throw std::invalid_argument("Different matrix dimentions");
+  }
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      matrix_[i][j] -= other.matrix_[i][j];
+    }
+  }
+}
+
+void S21Matrix::MulNumber(const double num) {
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      matrix_[i][j] *= num;
+    }
+  }
+}
+
+void S21Matrix::MulMatrix(const S21Matrix& other) {
+  if (cols_ != other.rows_) {
+    throw std::invalid_argument("Different matrix dimentions");
+  }
+  S21Matrix result(rows_, other.cols_);
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < other.cols_; j++) {
+      result.matrix_[i][j] = 0;
+      for (int k = 0; k < cols_; k++) {
+        result.matrix_[i][j] += matrix_[i][k] * other.matrix_[k][j];
+      }
+    }
+  }
+  *this = result;
+}
+
+S21Matrix S21Matrix::Transpose() {
+  S21Matrix result(rows_, cols_);
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      result.matrix_[i][j] = matrix_[j][i];
+    }
+  }
+  return result;
+}
+
+S21Matrix S21Matrix::CalcComplements() {
+  S21Matrix result(rows_, cols_);
+  return result;
+}
+
+double S21Matrix::Determinant() {
+  if (rows_ != cols_) {
+    throw std::invalid_argument("Different matrix dimentions");
+  }
+  S21Matrix tmp(*this);
+
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      tmp.matrix_[i][j] = matrix_[i][j];
+    }
+  }
+  mult = 0;
+  for (int i = 0; i < rows_; i++) {
+    for (int j = i + 1; j < rows_; j++) {
+      mult = tmp.matrix_[j][i] / tmp.matrix_[i][i];
+      for (int k = 0; k < rows_; k++) {
+        tmp.matrix_[j][k] -= mult * tmp.matrix_[i][k];
+      }
+    }
+  }
+  result = 1.0;
+  for (int i = 0; i < rows_; i++) {
+    result *= tmp.matrix_[i][i];
+  }
+  return result;
+}
+
+S21Matrix S21Matrix::CalcComplements() {
+  if (rows_ != cols_) {
+    throw std::invalid_argument("Different matrix dimensions");
+  }
+
+  if (rows_ == 1) {
+    return S21Matrix();  // Return a 1x1 matrix with value 1
+  }
+
+  S21Matrix tmp(*this);            // Create a copy of the current matrix
+  S21Matrix result(rows_, cols_);  // Create a matrix to store the complements
+
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      S21Matrix minorMatrix(rows_ - 1,
+                            cols_ - 1);  // Create a matrix for the minor
+      int minor_row = 0;
+
+      for (int row = 0; row < rows_; row++) {
+        if (row == i) {
+          continue;
+        }
+        int minor_col = 0;
+        for (int col = 0; col < cols_; col++) {
+          if (col == j) {
+            continue;
+          }
+          minorMatrix.matrix_[minor_row][minor_col] = tmp.matrix_[row][col];
+          minor_col++;
+        }
+        minor_row++;
+      }
+
+      double det =
+          minorMatrix.Determinant();  // Calculate determinant of the minor
+      double num = det * pow(-1, i + j);
+      result.matrix_[i][j] = num;  // Store the complement value
+    }
+  }
+
+  return result;
+}
 
 int main() {
   std::cout << "hello";
--- matrix.h
+++ matrix.h
@@ -9,18 +9,28 @@ class S21Matrix {
       int cols);  // Parametrized constructor with number of rows and columns
   S21Matrix(const S21Matrix& other);  // Copy constructor
   S21Matrix(S21Matrix&& other);       // Move constructor
-  void SumMatrix(const S21Matrix& other);
-  bool EqMatrix(const S21Matrix& other);
-  void SubMatrix(const S21Matrix& other);
-  void MulNumber(const double num);
-  void MulMatrix(const S21Matrix& other);
-  S21Matrix Transpose();
+  void SumMatrix(
+      const S21Matrix& other);  // Adds the second matrix to the current one
+  bool EqMatrix(
+      const S21Matrix& other);  // Checks matrices for equality with each other
+  void SubMatrix(
+      const S21Matrix& other);  // Subtracts another matrix from the current one
+  void MulNumber(
+      const double num);  // Multiplies the current matrix by a number
+  void MulMatrix(const S21Matrix& other);  // Multiplies the current matrix by
+                                           // the second matrix
+  S21Matrix Transpose();  // Creates a new transposed matrix from the current
+                          // one and returns it
   S21Matrix CalcComplements();
-  double Determinant();
+  double Determinant();  // Calculates and returns the determinant of the
+                         // current matrix
   S21Matrix InverseMatrix();
 
  private:
   // Attributes
   int rows_, cols_;  // Rows and columns
   double** matrix_;  // Pointer to the memory where the matrix is allocated
+  double determ;
+  double mult;
+  double result;
 };
